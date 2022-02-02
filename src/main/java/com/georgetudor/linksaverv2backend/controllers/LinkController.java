ee97534e93eb12a5c78719de805b015587b7502b
George Tudor <Alexander.George.Tudor@gmail.com>
2022-02-01 23:02:48 -0600

    q

---
+++ src/main/java/com/georgetudor/linksaverv2backend/controllers/LinkController.java
@@ -0,0 +1,20 @@
+package com.georgetudor.linksaverv2backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LinkController {
+
+    @PostMapping("/add-link")
+    public String addLink() {
+        return "add-link";
+    }
+
+    @GetMapping("/list-links")
+    public String listLinks() {
+        return "list-links";
+    }
+
+}
---
+++ src/main/java/com/georgetudor/linksaverv2backend/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.georgetudor.linksaverv2backend.repositories;
+
+import com.georgetudor.linksaverv2backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
