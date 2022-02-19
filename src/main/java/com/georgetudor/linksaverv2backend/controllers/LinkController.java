4489bdddda5b2eb7242962a508194d4f6926ed5c
George Tudor <Alexander.George.Tudor@gmail.com>
2022-02-19 16:08:44 -0600

    added OpenGraph parser, save description and image url to database

--- src/main/java/com/georgetudor/linksaverv2backend/controllers/LinkController.java
+++ src/main/java/com/georgetudor/linksaverv2backend/controllers/LinkController.java
@@ -1,10 +1,11 @@
 package com.georgetudor.linksaverv2backend.controllers;
 
 import com.georgetudor.linksaverv2backend.models.Link;
-import com.georgetudor.linksaverv2backend.models.LinkPreviewAPI.LinkPreview;
 import com.georgetudor.linksaverv2backend.repository.LinkRepository;
 import com.georgetudor.linksaverv2backend.repository.UserRepository;
 import com.georgetudor.linksaverv2backend.services.LinkPreviewService;
+import com.github.siyoon210.ogparser4j.OgParser;
+import com.github.siyoon210.ogparser4j.OpenGraph;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -22,14 +23,23 @@ public class LinkController {
     @Autowired
     private LinkPreviewService linkPreviewService;
 
+
+
     @PostMapping(value = "/links")
     public void saveLink(@RequestBody Link link, Authentication authentication) {
         link.setUser(userRepository.findByUsername(authentication.getName()));
-        LinkPreview linkPreview = linkPreviewService.getLinkInfo(link);
-        link.setTitle(linkPreview.getTitle());
-        link.setDescription(linkPreview.getDescription());
-        link.setImageUrl(linkPreview.getImage());
-        link.setUrl(linkPreview.getUrl());
+//        LinkPreview linkPreview = linkPreviewService.getLinkInfo(link);
+//        link.setTitle(linkPreview.getTitle());
+//        link.setDescription(linkPreview.getDescription());
+//        link.setImageUrl(linkPreview.getImage());
+//        link.setUrl(linkPreview.getUrl());
+        System.out.println(link.getUrl());
+        OgParser ogParser = new OgParser();
+        OpenGraph openGraph = ogParser.getOpenGraphOf(link.getUrl());
+        OpenGraph.Content image = openGraph.getContentOf("image");
+        link.setImageUrl(image.getValue());
+        OpenGraph.Content description = openGraph.getContentOf("description");
+        link.setDescription(description.getValue());
         linkRepository.save(link);
     }
 
---
+++ src/main/java/com/georgetudor/linksaverv2backend/services/OpenGraphService.java
@@ -0,0 +1,15 @@
+//package com.georgetudor.linksaverv2backend.services;
+//
+//import com.github.siyoon210.ogparser4j.OgParser;
+//import com.github.siyoon210.ogparser4j.OpenGraph;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class OpenGraphService {
+//    OgParser ogParser = new OgParser();
+//    OpenGraph openGraph = ogParser.getOpenGraphOf("https://google.com");
+//    OpenGraph.Content image = openGraph.getContentOf("image");
+//    String imageType = image.getExtraDataValueOf("type");
+//
+//}
