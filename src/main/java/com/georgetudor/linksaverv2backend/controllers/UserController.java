45a7aa35546491246c7fcc5b66d1b1e6ebe47edc
George Tudor <Alexander.George.Tudor@gmail.com>
2022-03-02 23:22:09 -0600

    created /me endpoint to check authentication

--- src/main/java/com/georgetudor/linksaverv2backend/controllers/UserController.java
+++ src/main/java/com/georgetudor/linksaverv2backend/controllers/UserController.java
@@ -1,8 +1,15 @@
 package com.georgetudor.linksaverv2backend.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class UserController {
 
+    @GetMapping("/me")
+    public String whoAmI(Authentication authentication) {
+        return authentication.getName();
+    }
 }
