399f8670bf71bcdf689731c56b55154cd6253f05
George Tudor <Alexander.George.Tudor@gmail.com>
2022-03-04 23:48:48 -0600

    fixed CORS error

--- src/main/java/com/georgetudor/linksaverv2backend/controllers/UserController.java
+++ src/main/java/com/georgetudor/linksaverv2backend/controllers/UserController.java
@@ -2,9 +2,10 @@ package com.georgetudor.linksaverv2backend.controllers;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@CrossOrigin
 @Controller
 public class UserController {
 
