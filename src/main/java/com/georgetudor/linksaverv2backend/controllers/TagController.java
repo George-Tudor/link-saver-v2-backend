bec0a815a7d66f2ccb34c6f4c26e9b04dabb9882
George Tudor <Alexander.George.Tudor@gmail.com>
2022-10-01 21:46:35 -0500

    added TagController and TagService to handle adding tags to a link

--- src/main/java/com/georgetudor/linksaverv2backend/controllers/TagController.java
+++ src/main/java/com/georgetudor/linksaverv2backend/controllers/TagController.java
@@ -1,43 +1,36 @@
-//package com.georgetudor.linksaverv2backend.controllers;
-//
-//import com.georgetudor.linksaverv2backend.models.Tag;
-//import com.georgetudor.linksaverv2backend.repository.LinkRepository;
-//import com.georgetudor.linksaverv2backend.repository.TagRepository;
-//import com.georgetudor.linksaverv2backend.repository.UserRepository;
-//import com.github.siyoon210.ogparser4j.OgParser;
-//import com.github.siyoon210.ogparser4j.OpenGraph;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@CrossOrigin
-//public class TagController {
-//
-//    @Autowired
-//    private LinkRepository linkRepository;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private TagRepository tagRepository;
-//
-//    @PostMapping(value = "/tags")
-//    public void addTag(@RequestBody Tag tag) {
-//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-//        tag.setUser(userRepository.findByUsername(username));
-//        OgParser ogParser = new OgParser();
-//        OpenGraph openGraph = ogParser.getOpenGraphOf(link.getUrl());
-//        OpenGraph.Content image = openGraph.getContentOf("image");
-//        link.setImageUrl(image.getValue());
-//        OpenGraph.Content description = openGraph.getContentOf("description");
-//        link.setDescription(description.getValue());
-//        OpenGraph.Content title = openGraph.getContentOf("title");
-//        link.setTitle(title.getValue());
-//        linkRepository.save(link);
-//    }
-//}
+package com.georgetudor.linksaverv2backend.controllers;
+
+import com.georgetudor.linksaverv2backend.models.Link;
+import com.georgetudor.linksaverv2backend.models.Tag;
+import com.georgetudor.linksaverv2backend.repository.LinkRepository;
+import com.georgetudor.linksaverv2backend.repository.TagRepository;
+import com.georgetudor.linksaverv2backend.repository.UserRepository;
+import com.georgetudor.linksaverv2backend.services.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class TagController {
+
+    @Autowired
+    private LinkRepository linkRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @Autowired
+    TagService tagService;
+
+
+    @ResponseBody
+    @RequestMapping(value = "/add-tag", method = RequestMethod.POST)
+    public void addTag(@RequestBody Tag tag, Link link) {
+        String linkUrl = link.getUrl();
+        tagService.addTag(tag, linkUrl);
+    }
+
+}
--- src/main/java/com/georgetudor/linksaverv2backend/services/TagService.java
+++ src/main/java/com/georgetudor/linksaverv2backend/services/TagService.java
@@ -1,7 +1,7 @@
 package com.georgetudor.linksaverv2backend.services;
 
+import com.georgetudor.linksaverv2backend.models.Link;
 import com.georgetudor.linksaverv2backend.models.Tag;
-import com.georgetudor.linksaverv2backend.models.User;
 import com.georgetudor.linksaverv2backend.repository.LinkRepository;
 import com.georgetudor.linksaverv2backend.repository.TagRepository;
 import com.georgetudor.linksaverv2backend.repository.UserRepository;
@@ -19,11 +19,36 @@ public class TagService {
     @Autowired
     TagRepository tagRepository;
 
-    public void addTag(Tag tag, String username) {
-        User user = userRepository.findByUsername(username);
-//        Tag existingTag = tagRepository.findById();
-//        if(existingTag == null) {
-//
-//        }
+    public void addTag(Tag tag, String linkUrl) {
+        Link link = linkRepository.findByUrl(linkUrl);
+        Tag existingTag = tagRepository.findTagByDescription(tag.getDescription());
+        if (existingTag == null) {
+            addNewTag(tag, link);
+        } else {
+            addExistingTag(existingTag, link);
+        }
+    }
+
+    public void addNewTag(Tag tag, Link link) {
+        Tag savedTag = tagRepository.save(tag);
+        addTagToLink(link, savedTag);
+    }
+
+    private void addTagToLink(Link link, Tag tag) {
+        link.getTag().add(tag);
+        linkRepository.save(link);
+    }
+
+    private void addExistingTag(Tag tag, Link link) {
+        Tag existingLinkTag = null;
+        for (Tag t : link.getTag()) {
+            if (t.getDescription().equals(tag.getDescription())) {
+                existingLinkTag = t;
+                break;
+            }
+        }
+        if (existingLinkTag == null) {
+            addTagToLink(link, tag);
+        }
     }
 }
